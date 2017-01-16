0ba2842872f91b7af1fe42ffb45726e4bf874c89
wangtianhang <690879430@qq.com>
2017-01-16 18:41:05 +0800

    测试高阶函数！

--- test.rkt
+++ test.rkt
@@ -112,3 +112,22 @@
   (= n (smallest-divisor n))
   )
 ;=================================
+
+;=========高阶函数抽象============
+(define (cube x) (* x x x))
+
+(define (inc n) (+ n 1))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b)
+         )
+      )
+  )
+
+(define (sum-cubes a b)
+  (sum cube a inc b)
+  )
+;==============================
