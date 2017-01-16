4208a7205c5df6daeaeeb0e694ebad8e91c8e91e
wangtianhang <690879430@qq.com>
2017-01-16 23:36:04 +0800

    部分表格映射

--- test.rkt
+++ test.rkt
@@ -180,4 +180,27 @@
 
 ;(define one-half (make-rat 1 2))
 ;(print-rat one-half)
+
+;(define one-through-four (list 1 2 3 4))
+;one-through-four
+(define (list-ref items n)
+  (if( = n 0)
+     (car items)
+     (list-ref (cdr items) (- n 1))
+     )
+  )
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))
+      )
+  )
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))
+      )
+  )
 ;===============end===============
