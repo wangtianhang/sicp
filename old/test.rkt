141a0ab7086673dc8220f4ebc0ba049d54570222
wth <690879430@qq.com>
2018-12-02 21:49:53 +0800

    整理工程路径

--- old/test.rkt
+++ old/test.rkt
@@ -259,12 +259,17 @@
 ;==========end======================
 
 ;=========存储状态=================
-(define balance 100)
-
-(define (withdraw amount)
-  (if(>= balance amount)
+(define new-withdraw
+  (let ((balance 100))
+  (lambda (amount)   
+    (if(>= balance amount)
      (begin (set! balance (- balance amount)) balance)
      "Insufficient funds"
      )
+    )
   )
+  )
+
+;(new-withdraw 25)
+;(new-withdraw 100)
 ;==========end================
